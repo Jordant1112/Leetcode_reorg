f78821fc16fd529121d05a1e99859376506bf2a0
Jordant1112 <jordant1112@gmail.com>
2024-10-20 16:52:27 -0700

    update LC 666

--- python/666PathSumIV/solution.py
+++ python/666PathSumIV/solution.py
@@ -1,25 +1,29 @@
 class Solution:
-    def pathSum(nums: List[int]) -> int:
+    def pathSum(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        res = 0
+        self.d = {}
         self.sum = 0
-        self.tree = {}
-        for code in nums:
-            val = code % 10
-            path = code // 10
-            self.tree[path] = val
-        path = nums[0] // 10
-        self.traverse(path, 0)
-        return self.sum
+        for n in nums:
+            val = n % 10
+            code = n // 10
+            self.d[code] = val
+        root_code = nums[0]//10
+        self.traverse(root_code, res)
+        return res
 
-    def traverse(self, path, res):
-        if path not in self.tree:
-            return
-        val = self.tree[path]
-        position = path % 10
-        level = path // 10
-        left_path = (level+1)*10 + position * 2 -1
-        right_path = (level+1) * 10 + position * 2
-        if left_path not in self.tree and right_path not in self.tree:
-            res += val + res
-            return
-        self.traverse(left_path, res + val)
-        self.traverse(right_path, res + val)
+    def traverse(self, root, res):
+        if not root:
+            return 
+        val = self.d[root]
+        code = root // 10
+        idx = root % 10
+        level = code // 10
+        left_code = (level + 1) * 10 + idx * 2 -1
+        right_cpde = (level+1) * 10 + idx * 2
+        if left_code not in self.d and right_code not in self.d:
+            self.sum += res + val
+            return 
+        self.traverse(left_code, res + val)
+        self.traverse(right_code, res + val)
